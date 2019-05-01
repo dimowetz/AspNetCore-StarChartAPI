c1c1dd07a38493ee6c8b2a4fdb9a407d74f0a47b
Dimo <bafmaamu@yahoo.com>
2019-05-01 20:11:12 +0200

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,65 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var currentObj = _context.CelestialObjects.FirstOrDefault(co => co.Id == id);
+            if (currentObj == null)
+            {
+                return NotFound();
+            }
+
+            var listOrbitals = _context.CelestialObjects.Where(o => o.OrbitedObjectId == currentObj.Id).ToList();
+            if (listOrbitals.Any())
+            {
+                currentObj.Satellites = listOrbitals;
+            }
+
+            return Ok(currentObj);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var resultList = _context.CelestialObjects.Where(co => co.Name == name).ToList();
+            if (!resultList.Any())
+            {
+                return NotFound();
+            }
+
+            foreach (var currentObject in resultList)
+            {
+                var listOrbitals = _context.CelestialObjects.Where(o => o.OrbitedObjectId == currentObject.Id).ToList();
+                if (listOrbitals.Any())
+                {
+                    currentObject.Satellites = listOrbitals;
+                }
+            }
+
+            return Ok(resultList);
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            var resultList = _context.CelestialObjects.ToList();
+            if (!resultList.Any())
+            {
+                return NotFound();
+            }
+
+            foreach (var currentObject in resultList)
+            {
+                var listOrbitals = _context.CelestialObjects.Where(o => o.OrbitedObjectId == currentObject.Id).ToList();
+                if (listOrbitals.Any())
+                {
+                    currentObject.Satellites = listOrbitals;
+                }
+            }
+
+            return Ok(resultList);
+        }
     }
 }
